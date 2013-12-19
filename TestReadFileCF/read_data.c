4eac5287387cc9020fddc03997ef43ce126ac067
Alan K. Duncan <alan@cocoa-factory.com>
2013-12-19 05:37:29 -0600

    documentation

--- TestReadFileCF/read_data.c
+++ TestReadFileCF/read_data.c
@@ -23,18 +23,21 @@ FILE *data_file_pointer(void)
 {
     FILE *fp = NULL;
     
+    //  get a reference to the main bundle
     CFBundleRef mainBundle = CFBundleGetMainBundle();
     if( mainBundle == NULL )
     {
         printf("unable to get main bundle\n");
         return NULL;
     }
+    //  get the CFURL reference to the file we're interested in
     CFURLRef dataFileURL = CFBundleCopyResourceURL(mainBundle, CFSTR("data"), CFSTR("txt"), NULL);
     if( dataFileURL == NULL )
     {
         printf("unable to locate data file\n");
         return NULL;
     }
+    //  since fopen works with paths, we need to convert CFURL reference to path
     CFStringRef path;
     if( !CFURLCopyResourcePropertyForKey(dataFileURL, kCFURLPathKey, &path, NULL))
     {
--- TestReadFileCF/read_data.h
+++ TestReadFileCF/read_data.h
@@ -3,7 +3,21 @@
 #include <stdio.h>
 #include <CoreFoundation/CFString.h>
 
+/**
+ *	File pointer to the file "data.txt"
+ *
+ *	@return	Returns a file pointer to the file "data.txt" if the file can be opened.  If the
+ *          file cannot be opened, or an error occurs, returns \p NULL.
+ */
 extern FILE *data_file_pointer(void);
+
+/**
+ *	C string from a CFString
+ *
+ *	@param	aString	The CFString to convert
+ *
+ *	@return	Returns a C string from the contents of the CFString provided.
+ */
 extern char * CFStringCopyUTF8String(CFStringRef aString);
 
 #endif
